5f341a1f5790d45efaf8b1a955179dc7e6d60cef
shimarrudz <vic.shima.vss@gmail.com>
2023-08-31 15:42:02 -0300

    feat: create vote view

--- home/mycode/mysite/polls/views.py
+++ home/mycode/mysite/polls/views.py
@@ -5,9 +5,11 @@ def index(request):
     return HttpResponse("Hello, world. You're at tue polls index.")
 
 def detail(request, question_id):
-    return HttpResponse("You 're looking at question %s." % question_id)
+    return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
-    response = "You 're looking at the results of question %s."
+    response = "You're looking at the results of question %s."
     return HttpResponse(response %  question_id)
 
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
