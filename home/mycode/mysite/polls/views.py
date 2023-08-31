cd5a6e912093c545e733255d404157a331cf8383
shimarrudz <vic.shima.vss@gmail.com>
2023-08-31 15:38:27 -0300

    feat: create detail view

--- home/mycode/mysite/polls/views.py
+++ home/mycode/mysite/polls/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at tue polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You 're looking at question %s." % question_id)
