90fc94882d8a324aad2ea3a4763b8fa0beeb0f67
shimarrudz <vic.shima.vss@gmail.com>
2023-08-31 16:07:09 -0300

    feat: create 404 exception on views

--- home/mycode/mysite/polls/views.py
+++ home/mycode/mysite/polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 from .models import Question
 
@@ -11,8 +11,11 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
