412e456b98df84036a92739323a6cc6990df1352
shimarrudz <vic.shima.vss@gmail.com>
2023-09-11 19:52:21 -0300

    chore: remove empty lines

--- home/mycode/mysite/polls/views.py
+++ home/mycode/mysite/polls/views.py
@@ -5,8 +5,6 @@ from django.views import generic
 
 from .models import Choice, Question
 
-
-
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
