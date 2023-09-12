3026bf8be0cf940ce34a4e704d80252b77b97d75
shimarrudz <vic.shima.vss@gmail.com>
2023-09-12 14:30:58 -0300

    test: add more tests

--- home/mycode/mysite/polls/models.py
+++ home/mycode/mysite/polls/models.py
@@ -17,4 +17,5 @@ class Choice(models.Model):
     def __str__(self):
         return self.choice_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
