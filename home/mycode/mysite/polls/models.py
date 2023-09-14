46e1aad31363c03ef154555d32d262602e57bdb5
shimarrudz <vic.shima.vss@gmail.com>
2023-09-14 15:58:09 -0300

    feat: improve models.py

--- home/mycode/mysite/polls/models.py
+++ home/mycode/mysite/polls/models.py
@@ -9,6 +9,15 @@ class Question(models.Model):
     pub_date = models.DateTimeField("date published")
     def __str__(self):
         return self.question_text
+    
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
